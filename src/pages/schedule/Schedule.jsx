15f1f4a051e66e6f8a447ec5684be47c06ed61cc
김용민 <dydals3440@gmail.com>
2024-01-12 01:25:24 +0900

    feat: Layout Change Button 제작

--- src/pages/schedule/Schedule.jsx
+++ src/pages/schedule/Schedule.jsx
@@ -1,16 +1,36 @@
+import { useState } from 'react';
+
 import * as S from './Schedule.style';
 import { Journey, TravelCalendar } from '@/components';
 import testData from '@/constants/journey';
 
 const SchedulePage = () => {
+  const [layout, setLayout] = useState('Calendar');
+
   return (
     <S.Container $dataLength={testData.length}>
-      <TravelCalendar />
-      <div>
-        {testData.map(item => (
-          <Journey key={item.id} data={item} dataLength={testData.length} />
-        ))}
-      </div>
+      <S.ButtonContainer>
+        <S.Button
+          clicked={layout === 'Calendar'}
+          onClick={() => setLayout('Calendar')}>
+          캘린더로 보기
+        </S.Button>
+        <S.Button clicked={layout === 'Map'} onClick={() => setLayout('Map')}>
+          지도로 보기
+        </S.Button>
+      </S.ButtonContainer>
+      {layout === 'Calendar' ? (
+        <S.CalendarWrapper>
+          <TravelCalendar />
+          <S.JourneyWrapper>
+            {testData.map(item => (
+              <Journey key={item.id} data={item} dataLength={testData.length} />
+            ))}
+          </S.JourneyWrapper>
+        </S.CalendarWrapper>
+      ) : (
+        <S.MapWrapper>Map</S.MapWrapper>
+      )}
     </S.Container>
   );
 };
--- src/pages/schedule/Schedule.style.js
+++ src/pages/schedule/Schedule.style.js
@@ -1,11 +1,47 @@
 import styled from 'styled-components';
 
+import theme from '@/theme';
+
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  ${theme.ALIGN.COLUMN_CENTER}
   gap: ${props => (props.$dataLength > 1 ? '0' : '10px')};
 `;
 
-export { Container };
+const ButtonContainer = styled.div`
+  ${theme.ALIGN.ROW_CENTER};
+  margin-top: 40px;
+  gap: 20px;
+`;
+
+const Button = styled.button`
+  width: 200px;
+  background-color: transparent;
+  padding: 20px;
+  border: ${props =>
+    props.clicked ? `2px solid ${theme.COLOR.MAIN.GREEN}` : 'none'};
+  border-radius: 40px;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+const CalendarWrapper = styled.div`
+  ${theme.ALIGN.COLUMN_CENTER}
+`;
+
+const JourneyWrapper = styled.div``;
+
+const MapWrapper = styled.div`
+  margin-top: 40px;
+`;
+
+export {
+  Container,
+  CalendarWrapper,
+  JourneyWrapper,
+  ButtonContainer,
+  Button,
+  MapWrapper,
+};
