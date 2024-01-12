0437f9f07615c1851582a94177f994602708328b
cowboysj <su10jin11@khu.ac.kr>
2024-01-12 23:58:41 +0900

    Feat : 시그니처 기록페이지

--- src/pages/signature/write/SignatureWrite.jsx
+++ src/pages/signature/write/SignatureWrite.jsx
@@ -1,5 +1,22 @@
+import styled from 'styled-components';
+
 const SignatureWritePage = () => {
-  return <div>SignatureWritePage</div>;
+  return (
+    <div>
+      <Title placeholder="제목" />
+    </div>
+  );
 };
 
 export default SignatureWritePage;
+
+const Title = styled.input`
+  outline: none;
+  border: none;
+  color: var(--black, #393939);
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
