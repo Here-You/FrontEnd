10ceb9d83864e930428a57a0b55802ee7662dcf0
chaKK <cdin23@naver.com>
2024-01-13 18:52:41 +0900

    DaliyRecordPage 구현

--- src/pages/dailyRecord/DailyRecordPage.jsx
+++ src/pages/dailyRecord/DailyRecordPage.jsx
@@ -1,13 +1,81 @@
+import { format } from 'date-fns';
+import { useEffect, useMemo, useState } from 'react';
+
 import * as S from './DailyRecordPage.style';
+import { testData } from './data';
+import ExpandLeft from '/icons/ExpandLeft.svg';
+import ExpandRight from '/icons/ExpandRight.svg';
 
 const DailyRecordPage = () => {
+  const [currentVisibleData, setCurrentVisibleData] = useState({});
+  const [slideDirection, setSlideDirection] = useState(null);
+  const [nowPage, setNowPage] = useState(3);
+
+  const date = useMemo(() => {
+    if (!currentVisibleData.date) {
+      return null;
+    }
+
+    return `${format(currentVisibleData.date, 'yyyy년 MM월 dd일')}`;
+  }, [currentVisibleData.date]);
+
+  useEffect(() => {
+    setCurrentVisibleData(testData[nowPage - 1]); //처음 불러온 데이터
+  }, [nowPage]);
+
+  const handleAnimationEnd = () => {
+    setSlideDirection(null);
+  };
+
+  const prevButtonClick = () => {
+    if (nowPage === 1) {
+      alert('이번 달 가장 첫 일지입니다!');
+    } else {
+      setSlideDirection('slide-right');
+      setNowPage(prevPage => prevPage - 1);
+    }
+  };
+
+  const nextButtonClick = () => {
+    if (nowPage === testData.length) {
+      alert('이번 달 가장 마지막 일지입니다!');
+    } else {
+      setSlideDirection('slide-left');
+      setNowPage(prevPage => prevPage + 1);
+    }
+  };
+
   return (
     <S.Container>
-      <S.DateContainer>
-        <div>2024</div>
-        <div>8, JAN</div>
-      </S.DateContainer>
-      <S.RecordContainer></S.RecordContainer>
+      <S.DateText>{date}의 일지</S.DateText>
+      <S.SlideContainer>
+        <div>
+          <S.RecordContainer></S.RecordContainer>
+        </div>
+        <div>
+          <S.MainRecordContainer>
+            <S.LeftButton src={ExpandLeft} onClick={prevButtonClick} />
+
+            <S.RecordContentsContainer
+              className={slideDirection}
+              onAnimationEnd={handleAnimationEnd}>
+              <S.PreviewImage src={currentVisibleData.recordImg} />
+              <S.LocationText>{currentVisibleData.location}</S.LocationText>
+              <S.TitleText>{currentVisibleData.title}</S.TitleText>
+              <S.WeatherContainer>
+                <S.WeatherText>{currentVisibleData.weather}</S.WeatherText>
+                <S.FeelingText>{currentVisibleData.feeling}</S.FeelingText>
+              </S.WeatherContainer>
+              <S.ContentText>{currentVisibleData.content}</S.ContentText>
+            </S.RecordContentsContainer>
+
+            <S.RightButton src={ExpandRight} onClick={nextButtonClick} />
+          </S.MainRecordContainer>
+        </div>
+        <div>
+          <S.RecordContainer></S.RecordContainer>
+        </div>
+      </S.SlideContainer>
     </S.Container>
   );
 };
--- src/pages/dailyRecord/DailyRecordPage.style.js
+++ src/pages/dailyRecord/DailyRecordPage.style.js
@@ -1,19 +1,179 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const slideAnimation = keyframes`
+  0% {
+    opacity: 0.5;
+    transform: translate3d(-10%, 0, 0);
+  }
+  to {
+    opacity: 1;
+    transform: translateZ(0);
+  }
+`;
+
+const slideAnimationReverse = keyframes`
+  0% {
+    opacity: 0.5;
+    transform: translate3d(10%, 0, 0);
+  }
+  to {
+    opacity: 1;
+    transform: translateZ(0);
+  }
+`;
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  gap: 10px;
+  align-items: center;
+  gap: 20px;
 `;
 
-const DateContainer = styled.div`
+const DateText = styled.p`
+  color: #333;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const SlideContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  overflow: hidden;
+  gap: 40px;
+`;
+
+const RecordContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  gap: 20px;
   align-items: center;
+  padding: 43px 40px;
+  min-width: 550px;
+  width: 100%;
+  height: 930px;
+  border: 1px solid #21b69c;
+  border-radius: 30px;
 `;
 
-const RecordContainer = styled.div``;
+const MainRecordContainer = styled(RecordContainer)`
+  position: relative;
+`;
 
-export { Container, DateContainer, RecordContainer };
+const RecordContentsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+  min-width: 550px;
+  width: 100%;
+
+  &.slide-left {
+    animation: ${slideAnimation} 0.55s;
+  }
+  &.slide-right {
+    animation: ${slideAnimationReverse} 0.55s;
+  }
+`;
+
+const RightButton = styled.img`
+  position: absolute;
+  width: 35px;
+  height: 35px;
+  bottom: 400px;
+  left: 635px;
+  cursor: pointer;
+
+  &:hover {
+    width: 40px;
+    height: 40px;
+  }
+`;
+
+const LeftButton = styled.img`
+  position: absolute;
+  width: 35px;
+  height: 35px;
+  bottom: 400px;
+  right: 635px;
+  cursor: pointer;
+
+  &:hover {
+    width: 40px;
+    height: 40px;
+  }
+`;
+
+const PreviewImage = styled.img`
+  display: flex;
+  width: 300px;
+  height: 250px;
+  border-radius: 20px;
+  object-fit: cover;
+`;
+
+const LocationText = styled.p`
+  display: flex;
+  height: 50px;
+  color: #333;
+  font-size: 15px;
+  font-weight: 400px;
+  text-align: center;
+  white-space: pre-wrap;
+`;
+
+const TitleText = styled.p`
+  color: black;
+  font-size: 30px;
+  font-weight: 700;
+  text-align: center;
+  white-space: pre-wrap;
+`;
+
+const WeatherContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  gap: 10px;
+  width: 350px;
+`;
+
+const WeatherText = styled.p`
+  color: #1b9c85;
+  font-size: 16px;
+  font-weight: 700;
+  text-align: center;
+`;
+
+const FeelingText = styled(WeatherText)``;
+
+const ContentText = styled.p`
+  color: black;
+  font-size: 17px;
+  font-weight: 700;
+  width: 100%;
+  height: 390px;
+  overflow: scroll;
+  text-align: center;
+  white-space: pre-wrap;
+`;
+
+export {
+  Container,
+  DateText,
+  SlideContainer,
+  RecordContainer,
+  MainRecordContainer,
+  RecordContentsContainer,
+  RightButton,
+  LeftButton,
+  PreviewImage,
+  LocationText,
+  TitleText,
+  FeelingText,
+  ContentText,
+  WeatherContainer,
+  WeatherText,
+};
