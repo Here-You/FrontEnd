4e792058b2d7392d81f7d6669e9632b2a15d8b12
cowboysj <su10jin11@khu.ac.kr>
2024-01-13 19:56:31 +0900

    Feat : 시그니처 메인페이지

--- src/pages/signature/Signature.jsx
+++ src/pages/signature/Signature.jsx
@@ -1,5 +1,60 @@
+import styled from 'styled-components';
+
+import Content from './main/Content';
+import Message from './main/Message';
+
 const SignaturePage = () => {
-  return <div>SignaturePage</div>;
+  const userName = 'cowboysj';
+  const profileUrl = 'https://i.ibb.co/JdGMYqf/Group-1000000912.png';
+
+  return (
+    <PageWrap>
+      <ProfileWrap>
+        <ProfileImg src={profileUrl} />
+        <UserName>{userName}</UserName>
+      </ProfileWrap>
+      <Message />
+      <Divider />
+      <Content />
+    </PageWrap>
+  );
 };
 
 export default SignaturePage;
+
+const PageWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  margin: 0 auto;
+  height: 100%;
+`;
+
+const ProfileWrap = styled.div`
+  display: flex;
+  height: 25%;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const ProfileImg = styled.img`
+  display: flex;
+  border-radius: 100%;
+  max-width: 40%;
+`;
+
+const UserName = styled.div`
+  display: flex;
+  font-family: 'Pretendard-bold';
+  font-size: 18px;
+`;
+
+const Divider = styled.div`
+  display: flex;
+  width: 90%;
+  height: 1px;
+  background: var(--gray2, #c8c8c8);
+`;
---
+++ src/pages/signature/main/MySignature.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import SignaturePreview from '../components/SignaturePreview';
+
+export default function MySignature() {
+  const MockData = {
+    userImgUrl: 'https://i.ibb.co/JdGMYqf/Group-1000000912.png',
+    date: '23 / 10 / 11',
+    previewUrl:
+      'https://i.ibb.co/D1bwz3j/Kakao-Talk-20231203-204945606.png" alt="Kakao-Talk-20231203-204945606',
+    title: '우리 동글이와 함꼐한, 강릉 1박 2일',
+    heart: 3012,
+  };
+  return (
+    <Wrap>
+      <SignaturePreview {...MockData} />
+      <SignaturePreview {...MockData} />
+    </Wrap>
+  );
+}
+
+const Wrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
