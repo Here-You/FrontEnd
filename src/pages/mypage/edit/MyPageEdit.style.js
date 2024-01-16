fc605f0b5bb0b1e00eb18e7494d92f0b2d5accd5
김용민 <dydals3440@gmail.com>
2024-01-17 03:49:59 +0900

    style: EdigPageContainer 배경색상 변경 LIGHT_GRAY 추가

--- src/pages/mypage/edit/MyPageEdit.style.js
+++ src/pages/mypage/edit/MyPageEdit.style.js
@@ -4,7 +4,7 @@ import { FONT_SIZE } from '@/constants/size';
 import theme from '@/theme';
 
 const EditPageContainer = styled.div`
-  background-color: ${theme.COLOR.MAIN.LIGHT_BLACK};
+  background-color: ${theme.COLOR.MAIN.LIGHT_GRAY};
   width: 100%;
   height: 100%;
   ${theme.ALIGN.COLUMN_CENTER}
