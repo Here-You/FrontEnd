62c9cdd78ae5efcea71c03c80fe6ab34ad168baa
00kang <kdb070970@gmail.com>
2024-01-18 16:28:43 +0900

    [Style]: 메이트 메인 페이지 스타일 수정

--- src/components/mate/Title.style.js
+++ src/components/mate/Title.style.js
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 import theme from '@/theme';
 
 const StyledSpan = styled.span`
-  ${theme.ALIGN.COLUMN_CENTER}
-  width: auto;
+  ${theme.ALIGN.COLUMN_CENTER};
+  align-items: flex-start;
+  width: 300px;
   height: 23px;
   color: ${theme.COLOR.MAIN.GREEN};
   font-size: 18px;
