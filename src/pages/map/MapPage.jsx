bb0341ae99c71c2da42491aed9c68eda24345f95
김용민 <dydals3440@gmail.com>
2024-01-22 19:43:36 +0900

    feat: ButtonContainer 디자인 수정

--- src/pages/map/MapPage.jsx
+++ src/pages/map/MapPage.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import * as S from './Map.style';
 import { TravelMap } from '@/components';
@@ -7,12 +7,19 @@ import testData from '@/constants/journey';
 
 const MapPage = () => {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   return (
     <S.Container $dataLength={testData.length}>
       <S.ButtonContainer>
-        <S.Button onClick={() => navigate('/')}>캘린더로 보기</S.Button>
-        <S.Button onClick={() => navigate('/map')}>지도로 보기</S.Button>
+        <S.Button clicked={pathname === '/'} onClick={() => navigate('/')}>
+          캘린더로 보기
+        </S.Button>
+        <S.Button
+          clicked={pathname === '/map'}
+          onClick={() => navigate('/map')}>
+          지도로 보기
+        </S.Button>
       </S.ButtonContainer>
       <S.MapContainer>
         <TravelMap />
