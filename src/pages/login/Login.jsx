67030bd81d8557247a70130dbbf1efbc3fa6fc3e
김용민 <dydals3440@gmail.com>
2024-01-22 21:54:33 +0900

    Revert "[Feat] : Login 메인"

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,18 +1,5 @@
-import * as S from './Login.style';
-import Logo from '/images/main.svg';
-import LoginButton from '@/components/login/LoginButton';
-
 const LoginPage = () => {
-  return (
-    <S.LoginContainer>
-      <S.LogoImg src={Logo} />
-      <S.ButtonWrap>
-        <S.LinkTo to="/signup">
-          <LoginButton />
-        </S.LinkTo>
-      </S.ButtonWrap>
-    </S.LoginContainer>
-  );
+  return <div>LoginPage</div>;
 };
 
 export default LoginPage;
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,14 +1,5 @@
-import SignUpMyInfo from '@/components/signup/SignUpMyInfo';
-import * as S from './SignUp.style'
-
-
 const SignUpPage = () => {
-  return (
-    <S.SignUpContainer>
-      <h2>내 정보</h2>
-      <SignUpMyInfo />
-    </S.SignUpContainer>
-  );
+  return <div>SignUpPage</div>;
 };
 
 export default SignUpPage;
