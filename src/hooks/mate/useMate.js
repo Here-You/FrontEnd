84aee7d6eb3b485efcbaf4fb4bca3c3326440fa4
00kang <kdb070970@gmail.com>
2024-01-27 17:39:55 +0900

    [Feat] : useMateLook hook 구현 및 사용

---
+++ src/hooks/mate/useMate.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+import { getMateLook } from '@/apis/request/mate';
+
+export const useMateLook = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await getMateLook();
+
+        const data = res.data;
+
+        setData(data);
+      } catch (e) {
+        setError(e.message || '에러가 발생했습니다.');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { data, loading, error };
+};
--- src/pages/mate/look/MateLook.jsx
+++ src/pages/mate/look/MateLook.jsx
@@ -4,21 +4,10 @@ import * as S from './MateLook.style';
 import mateSearchIcon from '/images/mate_search.svg';
 import { getMateLook } from '@/apis/request/mate';
 import MateSignatureSection from '@/components/mate/MateSignatureSection';
+import { useMateLook } from '@/hooks/mate/useMate';
 
 const MateLookPage = () => {
-  const [matesData, setMatesData] = useState([]);
-
-  useEffect(() => {
-    const fetchMateData = async () => {
-      try {
-        const response = await getMateLook();
-        setMatesData(response.data);
-      } catch (error) {
-        console.error('데이터를 가져오는데 실패했습니다.', error);
-      }
-    };
-    fetchMateData();
-  }, []);
+  const { data: matesData, loading, error } = useMateLook();
 
   return (
     <>
@@ -33,15 +22,20 @@ const MateLookPage = () => {
           <S.StyledIcon src={mateSearchIcon}></S.StyledIcon>
         </S.FixedContainer>
 
-        <MateSignatureSection
-          text="샐리님이 사용한 위치 [#오사카]를 함께 이용 중인 메이트"
-          matesData={matesData}
-        />
-
-        <MateSignatureSection
-          text="샐리님의 메이트가 작성한 시그니처"
-          matesData={matesData}
-        />
+        {loading ? (
+          <div>로딩중입니다.</div>
+        ) : (
+          <>
+            <MateSignatureSection
+              text="샐리님이 사용한 위치 [#오사카]를 함께 이용 중인 메이트"
+              matesData={matesData}
+            />
+            <MateSignatureSection
+              text="샐리님의 메이트가 작성한 시그니처"
+              matesData={matesData}
+            />
+          </>
+        )}
       </S.CenteredContainer>
     </>
   );
