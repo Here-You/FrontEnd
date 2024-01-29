48e7171262cc8d9d66346a82a490de68ed3ee490
kimjunha <tto9011@naver.com>
2024-01-30 01:12:26 +0900

    [Feat]: 반응형 조정

--- src/components/mypage/MypageInfo.style.js
+++ src/components/mypage/MypageInfo.style.js
@@ -1,62 +1,78 @@
-import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
 import { FONT_SIZE } from '@/constants/size';
 import theme from '@/theme';
 
 const ProfileContainer = styled.div`
   width: 100%;
-  padding: 40px;
+  padding: 40px 0 40px 50px;
   border: 2px solid ${theme.COLOR.MAIN.LIGHT_BLACK};
   border-radius: 30px;
   display: flex;
-  align-items: self-start;
+  justify-content: start;
+
+  @media ${theme.WINDOW_SIZE.MOBILE} {
+    padding: 20px 20px;
+  }
 
   h3 {
     font-size: ${FONT_SIZE.TWO_XL};
     font-weight: 700;
 
     @media ${theme.WINDOW_SIZE.MOBILE} {
-      font-size: ${FONT_SIZE.XL};
+      font-size: ${FONT_SIZE.BASE};
     }
   }
-
   p {
     color: ${theme.COLOR.MAIN.GRAY};
     font-size: ${FONT_SIZE.LG};
     font-weight: 350;
 
     @media ${theme.WINDOW_SIZE.MOBILE} {
-      font-size: ${FONT_SIZE.BASE};
+      font-size: ${FONT_SIZE.XS};
     }
   }
 `;
 
 const ProfileInfoContainer = styled.div`
   ${theme.ALIGN.COLUMN_CENTER};
+  margin-left: 50px;
+  width: 100%;
   align-items: start;
+
   line-height: 35px;
-  margin-left: 40px;
+  @media ${theme.WINDOW_SIZE.MOBILE} {
+    line-height: 20px;
+    margin-left: 30px;
+  }
 `;
 
 const NickNameTypeContainer = styled.div`
   display: flex;
-  width: 50%;
+
+  width: 30%;
   justify-content: space-between;
+
+  @media ${theme.WINDOW_SIZE.MOBILE} {
+    width: 40%;
+  }
 `;
 
 const ProfilePicture = styled.img`
-  width: 90px;
-  border-radius: 50%;
+  width: 23%;
+  height: 1%;
 `;
 
 const Mate = styled.div`
   color: ${theme.COLOR.MAIN.GRAY};
   font-weight: 350;
   font-size: ${FONT_SIZE.LG};
-  margin-top: 20px;
+  margin-top: 15px;
 
   @media ${theme.WINDOW_SIZE.MOBILE} {
-    font-size: ${FONT_SIZE.BASE};
+    font-size: ${FONT_SIZE.XS};
+    margin-top: 0;
   }
 `;
 
@@ -67,13 +83,13 @@ const NumberOfPeople = styled.span`
   margin: 0 10px;
 
   @media ${theme.WINDOW_SIZE.MOBILE} {
-    font-size: ${FONT_SIZE.BASE};
+    font-size: ${FONT_SIZE.XS};
   }
 `;
 
 const LinkTo = styled(Link)`
- text-decoration-line: none;
- color: ${theme.COLOR.MAIN.BLACK};
+  text-decoration-line: none;
+  color: ${theme.COLOR.MAIN.BLACK};
 `;
 
 export {
