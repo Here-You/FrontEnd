945d829bafeef6b743ed394255ee3178e92ac35d
김용민 <dydals3440@gmail.com>
2024-02-01 18:54:59 +0900

    Feat: Detail Journey 가운데 정렬

--- src/pages/journey/JourneyDetail.style.js
+++ src/pages/journey/JourneyDetail.style.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
-const Container = styled.div``;
+import theme from '@/theme';
+
+const Container = styled.div`
+  ${theme.ALIGN.COLUMN_CENTER}
+`;
 
 export { Container };
