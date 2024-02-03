fb691cffcda431eccd5ca99356208ee594633037
김용민 <dydals3440@gmail.com>
2024-02-04 02:14:57 +0900

    feat: MateLook Page InfiniteScroll

--- src/pages/mate/look/MateLook.style.js
+++ src/pages/mate/look/MateLook.style.js
@@ -1,15 +1,106 @@
 import styled from 'styled-components';
 
+import { FONT_SIZE } from '@/constants/size';
 import theme from '@/theme';
 
 const MateLookContainer = styled.div`
-  ${theme.ALIGN.COLUMN_CENTER};
-  position: relative;
+  gap: 10px;
+`;
+
+const Title = styled.h1`
+  padding: 0 25px;
+  font-size: ${FONT_SIZE.BASE};
+  color: ${theme.COLOR.MAIN.GRAY};
 `;
 
 const CenteredContainer = styled.div`
-  ${theme.ALIGN.COLUMN_CENTER};
-  margin: 0 20px;
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  width: 100%;
+  overflow-x: scroll;
+  margin-top: 20px;
+  padding: 0 20px;
 `;
 
-export { MateLookContainer, CenteredContainer };
+const MateContainer = styled.div`
+  ${theme.ALIGN.ROW_CENTER};
+  gap: 10px;
+`;
+
+const MateBox = styled.div`
+  ${theme.ALIGN.COLUMN_CENTER};
+  position: relative;
+  width: 250px;
+  height: 250px;
+  border: 1px solid ${theme.COLOR.MAIN.LIGHT_BLACK};
+  border-radius: 20px;
+  gap: 10px;
+`;
+
+const MateDescriptionBox = styled.div`
+  ${theme.ALIGN.ROW_CENTER};
+  gap: 10px;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  padding: 10px;
+  margin-left: 15px;
+  margin-top: 60px;
+  gap: 10px;
+`;
+
+const TextBox = styled.div`
+  ${theme.ALIGN.COLUMN_CENTER};
+  align-items: flex-start;
+
+  h1 {
+    font-size: 1.4rem;
+  }
+
+  p {
+    font-size: 0.8rem;
+  }
+`;
+
+const MateImage = styled.img`
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
+  border-radius: 50%;
+`;
+
+const SignatureContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  p {
+    font-size: 0.8rem;
+  }
+`;
+
+const SignatureImage = styled.img`
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  border-radius: 5px;
+`;
+
+export {
+  Title,
+  MateLookContainer,
+  CenteredContainer,
+  MateContainer,
+  MateDescriptionBox,
+  MateBox,
+  TextBox,
+  MateImage,
+  SignatureContainer,
+  SignatureImage,
+  ImageContainer,
+};
