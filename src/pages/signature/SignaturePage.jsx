7e0913685f234c0bb9a61ae8c713e14d4983ab49
김용민 <dydals3440@gmail.com>
2024-02-05 04:41:24 +0900

    feat: addPage 연결

--- src/pages/signature/SignaturePage.jsx
+++ src/pages/signature/SignaturePage.jsx
@@ -12,6 +12,7 @@ export default function SignaturePage() {
   const handleHeaderClick = header => {
     setSelectedHeader(header);
   };
+  const { addPage } = useSignatureWrite();
 
   return (
     <>
