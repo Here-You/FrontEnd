ee15ca7e9b640b410674cb5534966c55de6f090e
김용민 <dydals3440@gmail.com>
2024-02-07 00:34:10 +0900

    feat: 시그니처 핸들러 수정

--- src/mocks/handlers/signature.js
+++ src/mocks/handlers/signature.js
@@ -13,42 +13,42 @@ export const SignatureHandlers = [
       message: '내 시그니처 가져오기 성공',
       data: [
         {
-          id: 1,
+          _id: 1,
           title: '호주와 겨룬 8강전 직관 후기',
           date: '2024-02-03T08:03:22.000Z',
           image:
             'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         },
         {
-          id: 2,
+          _id: 2,
           title: '강남역 근처 카페 모음',
           date: '2024-02-03T08:14:40.000Z',
           image:
             'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         },
         {
-          id: 3,
+          _id: 3,
           title: '호주와 겨룬 8강전 직관 후기',
           date: '2024-02-03T08:03:22.000Z',
           image:
             'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         },
         {
-          id: 4,
+          _id: 4,
           title: '강남역 근처 카페 모음',
           date: '2024-02-03T08:14:40.000Z',
           image:
             'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         },
         {
-          id: 5,
+          _id: 5,
           title: '호주와 겨룬 8강전 직관 후기',
           date: '2024-02-03T08:03:22.000Z',
           image:
             'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         },
         {
-          id: 6,
+          _id: 6,
           title: '강남역 근처 카페 모음',
           date: '2024-02-03T08:14:40.000Z',
           image:
@@ -65,41 +65,10 @@ export const SignatureHandlers = [
       code: 'CREATED',
       success: true,
       message: '시그니처 기록하기 성공',
-      data: {
-        title: '새로운 시그니처 제목',
-        pages: [
-          {
-            content: '오늘은 호수 공원을 산책했습니다 어쩌구',
-            location: '광교 호수 공원',
-            page: 1,
-            image:
-              'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          },
-          {
-            content: '저는 원래 호수 산책을 참 좋아하는데요 어쩌구',
-            location: '동백 호수 공원',
-            page: 2,
-            image:
-              'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          },
-          {
-            content: '오늘은 호수 공원을 산책했습니다 어쩌구',
-            location: '광교 호수 공원',
-            page: 3,
-            image:
-              'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          },
-          {
-            content: '저는 원래 호수 산책을 참 좋아하는데요 어쩌구',
-            location: '동백 호수 공원',
-            page: 4,
-            image:
-              'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          },
-        ],
-      },
+      data: 2,
     });
   }),
+
   //시그니처_시그니처 상세보기
   //내 시그니처 Response-Body X, 다른 사람의 시그니처 Response-Body 만 O
   //내가 작성한 거 추가해야함
@@ -131,6 +100,7 @@ export const SignatureHandlers = [
           },
           pages: [
             {
+              _id: 1,
               image:
                 'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
               location: '리라쿠마 카페, 긴자',
@@ -138,6 +108,7 @@ export const SignatureHandlers = [
               page: 1,
             },
             {
+              _id: 2,
               image:
                 'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
               location: '서울, 강동',
@@ -145,6 +116,7 @@ export const SignatureHandlers = [
               page: 2,
             },
             {
+              _id: 3,
               image:
                 'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
               location: '경기도, 안산',
@@ -152,6 +124,7 @@ export const SignatureHandlers = [
               page: 3,
             },
             {
+              _id: 4,
               image:
                 'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
               location: '서울, 강남',
@@ -170,51 +143,29 @@ export const SignatureHandlers = [
     `${baseURL}${API_URL.GET_DETAIL_SIGNATURE}`,
     ({ request, params }) => {
       return HttpResponse.json({
-        title: '새로운 시그니처 제목',
-        pages: [
-          {
-            content: '오늘은 호수 공원을 산책했습니다 어쩌구',
-            location: '광교 호수 공원',
-            page: 1,
-            image:
-              'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          },
-          {
-            content: '저는 원래 호수 산책을 참 좋아하는데요 어쩌구',
-            location: '동백 호수 공원',
-            page: 2,
-            image:
-              'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          },
-          {
-            content: '오늘은 호수 공원을 산책했습니다 어쩌구',
-            location: '광교 호수 공원',
-            page: 3,
-            image:
-              'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          },
-          {
-            content: '저는 원래 호수 산책을 참 좋아하는데요 어쩌구',
-            location: '동백 호수 공원',
-            page: 4,
-            image:
-              'https://images.unsplash.com/photo-1671920090611-9a40303b52cb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          },
-        ],
+        timestamp: '2024-02-05T07:37:55.770Z',
+        code: 'OK',
+        success: true,
+        message: '시그니처 수정하기 성공',
+        data: '6',
       });
     },
   ),
+
   //시그니처_시그니처 삭제하기
   http.delete(
     `${baseURL}${API_URL.GET_DETAIL_SIGNATURE}`,
     ({ request, params }) => {
       return HttpResponse.json({
-        status: 204,
+        timestamp: '2024-02-05T07:40:21.691Z',
+        code: 'OK',
         success: true,
-        message: '시그니처 삭제하기 성공',
+        message: '시그니처 삭제 성공',
+        data: null,
       });
     },
   ),
+
   //시그니처_시그니처 좋아요, 좋아요 취소
   http.patch(`${baseURL}${API_URL.LIKE_SIGNATURE}`, ({ request, params }) => {
     return HttpResponse.json({
@@ -229,8 +180,39 @@ export const SignatureHandlers = [
     });
   }),
 
-  //시그니처_시그니처 좋아요한 사용자 목록 (API명세서 미완성)
-  // http.get(`${baseURL}${API_URL.LIKE_SIGNATURE}`, ({ request, params }) => {
-  //   return HttpResponse.json();
-  // }),
+  //시그니처_시그니처 좋아요한 사용자 목록
+  http.get(`${baseURL}${API_URL.LIKELIST_SIGNATURE}`, ({ request, params }) => {
+    return HttpResponse.json({
+      timestamp: '2024-02-05T15:03:14.672Z',
+      code: 'OK',
+      success: true,
+      message: '시그니처 좋아요 목록 불러오기 성공',
+      data: {
+        liked: 3,
+        profiles: [
+          {
+            _id: 1,
+            image: 'imagekey1',
+            introduction: '사용자 소개',
+            nickname: '완두콩',
+            is_followed: null,
+          },
+          {
+            _id: 2,
+            image: 'imagekey2',
+            introduction: '사용자 소개',
+            nickname: '강낭콩',
+            is_followed: false,
+          },
+          {
+            _id: 3,
+            image: 'imagekey3',
+            introduction: '사용자 소개',
+            nickname: '검은콩',
+            is_followed: false,
+          },
+        ],
+      },
+    });
+  }),
 ];
