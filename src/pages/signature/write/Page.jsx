a14afd93a3bd75d2f8b32bdf8c8263a54080a20e
김용민 <dydals3440@gmail.com>
2024-02-07 04:41:54 +0900

    feat: Error Page 제작

--- src/pages/signature/write/Page.jsx
+++ src/pages/signature/write/Page.jsx
@@ -26,9 +26,9 @@ export default function Page({ image, content }) {
         <S.Icon src={LocationLight} />
         <SearchMap
           pageIndex={currentPageIndex}
-          inputValue={pages[currentPageIndex].location}
+          inputValue={pages[currentPageIndex]?.location}
           selectLocation={info => {
-            updatePage(currentPageIndex, { location: info.name });
+            updatePage(currentPageIndex, { location: info?.name });
           }}
           placeholder="위치 입력"
         />
@@ -46,7 +46,7 @@ export default function Page({ image, content }) {
       </S.InputWrap>
       <S.ContentInput
         placeholder="오늘의 시그니처를 기록해보세요!"
-        value={content || pages[currentPageIndex].content}
+        value={content || pages[currentPageIndex]?.content}
         onChange={handleContentChange}
       />
     </S.PageContainer>
