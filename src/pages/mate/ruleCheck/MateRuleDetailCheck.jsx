45864997e77e5cf97be776398a3359cd7c1a70ee
김용민 <dydals3440@gmail.com>
2024-02-10 10:42:25 +0900

    feat: Rule-Check 디테일 페이지 데이터 연결

--- src/pages/mate/ruleCheck/MateRuleDetailCheck.jsx
+++ src/pages/mate/ruleCheck/MateRuleDetailCheck.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import * as S from './MateRuleDetailCheck.style';
+import Logo from '/images/mypage/MyPageLogo.svg';
 import MateCommentList from '@/components/comment/mate/MateCommentList';
 import MateCommentInput from '@/components/comment/mate/commentInput/MateCommentInput';
 import { useTeamMateRulePost } from '@/hooks/mate/useTeamMateRulePost';
@@ -10,11 +11,8 @@ const MateRuleDetailCheckPage = () => {
   const [editMode, setEditMode] = useState(false);
   const { ruleId } = useParams();
   const { data, loading, error } = useTeamMateRulePost(ruleId);
-  const titleRefs = useRef([]);
-  const detailRefs = useRef([]);
-  const navigate = useNavigate();
 
-  // console.log(titleRefs.current[1]);
+  const navigate = useNavigate();
 
   if (loading) {
     return <div>로딩 중 입니다..</div>;
@@ -24,49 +22,25 @@ const MateRuleDetailCheckPage = () => {
     return <div>에러가 발생했습니다.</div>;
   }
 
-  const handleSubmit = () => {
-    const editedContent = {
-      mainTitle: data[0]?.mainTitle || '',
-      rulePairs:
-        data[0]?.rulePairs?.map((rule, index) => ({
-          ruleTitle: titleRefs.current[index]?.innerText || '',
-          ruleDetail: detailRefs.current[index]?.innerText || '',
-        })) || [],
-    };
-
-    console.log('수정된 콘텐츠', editedContent);
-  };
-
   return (
     <S.Container>
       <S.Wrapper>
         <S.Header>
-          <S.Title>{data[0]?.mainTitle}</S.Title>
+          <S.Title>{data?.mainTitle}</S.Title>
           <S.ProfileContainer>
-            {data[0]?.detailMembers?.map(p => (
-              <S.ProfileBox key={p.memberId}>
-                <S.ProfileImages src={p.image} />
+            {data?.detailMembers?.map(p => (
+              <S.ProfileBox key={p.id}>
+                <S.ProfileImages src={p.image ? p.image : Logo} />
                 <h3>{p.name}</h3>
               </S.ProfileBox>
             ))}
           </S.ProfileContainer>
         </S.Header>
-
         <S.Content>
-          {data[0]?.rulePairs?.map((rule, index) => (
+          {data?.rulePairs?.map((rule, index) => (
             <S.TextContainer key={rule.id}>
-              <h3
-                contentEditable={editMode}
-                suppressContentEditableWarning={editMode}
-                ref={el => (titleRefs.current[index] = el)}>
-                {rule.ruleTitle}
-              </h3>
-              <p
-                contentEditable={editMode}
-                suppressContentEditableWarning={editMode}
-                ref={el => (detailRefs.current[index] = el)}>
-                {rule.ruleDetail}
-              </p>
+              <h3>{rule.ruleTitle}</h3>
+              <p>{rule.ruleDetail}</p>
             </S.TextContainer>
           ))}
         </S.Content>
@@ -80,7 +54,6 @@ const MateRuleDetailCheckPage = () => {
           저장하기
         </S.UpdateBtn>
       ) : (
-        // <S.UpdateBtn onClick={() => setEditMode(true)}>수정하기</S.UpdateBtn>
         <S.UpdateBtn onClick={() => navigate(`/mate/rule-edit/${ruleId}`)}>
           수정하기
         </S.UpdateBtn>
