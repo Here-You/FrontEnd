273ccd24acfc5ac5302589fbc78667aad3c2c608
김용민 <dydals3440@gmail.com>
2024-02-14 04:02:21 +0900

    feat: 대댓글 수정 삭제 기능 개발 완료

--- src/components/comment/signature/commentView/SignatureComment.jsx
+++ src/components/comment/signature/commentView/SignatureComment.jsx
@@ -1,27 +1,40 @@
 import { format } from 'date-fns';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import * as S from './SignatureComment.style';
 import Pen from '/icons/Pen.svg';
 import Trash from '/icons/Trash.svg';
 import Logo from '/images/mypage/MyPageLogo.svg';
-import { postSignatureReComment } from '@/apis/request/signature';
+import {
+  deleteSignatureReComment,
+  postSignatureReComment,
+  updateSignatureReComment,
+} from '@/apis/request/signature';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const SignatureComment = ({ data }) => {
   const { signatureId } = useParams();
-
   const queryClient = useQueryClient();
   const { _id, content, parentId, is_edited, writer, date } = data;
+
   const formattedEndDate = format(date, 'yyyy-MM-dd HH:mm:ss');
   const [editMode, setEditMode] = useState(false);
   const [isReplying, setIsReplying] = useState(false);
   const [replyComment, setReplyComment] = useState('');
-  console.log(replyComment);
+  const editModeRef = useRef(false);
+  const editedContentRef = useRef(content);
+  const inputRef = useRef(null);
 
-  const handleCommentEdit = () => {};
-  const handleCommentDelete = () => {};
+  useEffect(() => {
+    if (editModeRef.current) {
+      inputRef.current.focus();
+    }
+  }, [editModeRef.current]);
+
+  const handleContentChange = () => {
+    editedContentRef.current = inputRef.current.innerText;
+  };
 
   const indentationLevel = parentId === _id ? 0 : 1;
   const isParentComment = parentId === _id;
@@ -40,21 +53,79 @@ const SignatureComment = ({ data }) => {
     },
   });
 
+  const { mutateAsync: updateReComment } = useMutation({
+    mutationFn: updateSignatureReComment,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['signatureComments']);
+    },
+    onError: error => {
+      console.error('답글 수정 실패', error);
+    },
+  });
+
+  const { mutateAsync: deleteReComment } = useMutation({
+    mutationFn: deleteSignatureReComment,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['signatureComments']);
+    },
+    onError: error => {
+      console.error('답글 삭제 실패', error);
+    },
+  });
+
   return (
     <S.Container indentationLevel={indentationLevel}>
       <S.Avatar src={writer?.image ? writer?.image : Logo} />
       <S.ContentContainer>
         <S.NameContainer>
           <S.Name>{writer?.name}</S.Name>
-          {isParentComment && (
+          {writer?.is_writer === true && (
             <S.LeftContent>
               <S.Button onClick={handleReplyToggle}>답글</S.Button>
-              <S.Icon src={Pen} onClick={handleCommentEdit} />
-              <S.Icon src={Trash} onClick={handleCommentDelete} />
+              {editMode ? (
+                <button
+                  onClick={async () => {
+                    try {
+                      await updateReComment({
+                        signatureId,
+                        commentId: _id,
+                        content: editedContentRef.current,
+                      });
+                      setEditMode(false);
+                    } catch (e) {
+                      console.error(e);
+                    }
+                  }}>
+                  수정 완료
+                </button>
+              ) : (
+                <S.Icon
+                  src={Pen}
+                  onClick={() => {
+                    setEditMode(true);
+                  }}
+                />
+              )}
+              <S.Icon
+                src={Trash}
+                onClick={async () => {
+                  try {
+                    await deleteReComment({ signatureId, commentId: _id });
+                  } catch (e) {
+                    console.error(e);
+                  }
+                }}
+              />
             </S.LeftContent>
           )}
         </S.NameContainer>
-        <S.Content>{content}</S.Content>
+        <S.Content
+          contentEditable={editMode}
+          suppressContentEditableWarning={editMode}
+          ref={inputRef}
+          onInput={handleContentChange}>
+          {content}
+        </S.Content>
         <S.ContentInner>
           <S.Content>{formattedEndDate}</S.Content>
           <S.EditContent>{is_edited === true ? '수정됨' : null}</S.EditContent>
