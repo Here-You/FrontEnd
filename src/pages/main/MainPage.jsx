90ebb4cba3f5f477d434d0035c79325d874984ab
cowboysj <su10jin11@khu.ac.kr>
2024-02-13 14:09:35 +0900

    Style : 메인페이지 Style 수정

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,5 +1,4 @@
-import styled from 'styled-components';
-
+import * as S from './Mainpage.style';
 import Box from '@/components/main/Box';
 import HereyouBanner from '@/components/main/HereyouBanner';
 import HotSignature from '@/components/main/HotSignature';
@@ -8,32 +7,15 @@ import MapContainer from '@/components/main/MapContainer';
 
 export default function MainPage() {
   return (
-    <PageContainer>
+    <S.PageContainer>
       <LoginBanner />
       <MapContainer />
-      <Container2>
+      <S.Container2>
         <Box title="시그니처" />
         <Box title="메이트" />
-      </Container2>
+      </S.Container2>
       <HereyouBanner />
       <HotSignature />
-    </PageContainer>
+    </S.PageContainer>
   );
 }
-
-const PageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-  background-color: #f7f7f7;
-`;
-const Container2 = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 90%;
-  margin: 0 auto;
-  height: 25%;
-  justify-content: space-between;
-  margin-bottom: 20px;
-`;
---
+++ src/pages/main/Mainpage.style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+import theme from '@/theme';
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: #f7f7f7;
+`;
+const Container2 = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 90%;
+  margin: 0 auto;
+  height: 25%;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+export { PageContainer, Container2 };
