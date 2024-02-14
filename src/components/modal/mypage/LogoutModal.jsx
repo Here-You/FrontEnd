541de034325a530368de5c9924c03e97e1f5c158
김용민 <dydals3440@gmail.com>
2024-02-14 23:00:01 +0900

    feat: LogOut Modal 수정

--- src/components/modal/mypage/LogoutModal.jsx
+++ src/components/modal/mypage/LogoutModal.jsx
@@ -1,21 +1,21 @@
-import { useState } from 'react';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
 import Modal from '../Modal';
 import * as S from './BasicModal.style';
 import useLogoutModal from '@/hooks/modal/useLogoutModal';
+import useAuth from '@/store/useAuth';
 
 const LogoutModal = ({}) => {
   const logoutModal = useLogoutModal();
-  const [isLoading, setIsLoading] = useState(false);
-
+  const { logout } = useAuth();
   const navigate = useNavigate();
+
   const BodyContent = (
     <S.Container>
       <S.ContentContainer>
         <S.Title>여행의 이유 로그아웃</S.Title>
-        <S.Content>로그아웃 하시겠습니까?</S.Content>
+        <S.Content>정말로, 로그아웃 하시겠습니까?</S.Content>
       </S.ContentContainer>
     </S.Container>
   );
@@ -25,8 +25,7 @@ const LogoutModal = ({}) => {
   };
 
   const onSubmit = () => {
-    setIsLoading(true);
-    localStorage.removeItem('x-access-token');
+    logout();
     handleCloseModal();
     navigate('/login');
     toast.success('로그아웃 성공');
@@ -34,7 +33,6 @@ const LogoutModal = ({}) => {
 
   return (
     <Modal
-      disabled={isLoading}
       isOpen={logoutModal.isOpen}
       onClose={handleCloseModal}
       onSubmit={onSubmit}
--- src/components/mypage/MypageInfo.jsx
+++ src/components/mypage/MypageInfo.jsx
@@ -1,7 +1,9 @@
 import { useNavigate } from 'react-router-dom';
 
+import LogoutModal from '../modal/mypage/LogoutModal';
 import * as S from './MypageInfo.style';
 import { myPageImg } from '/public/images/mypage/index';
+import useLogoutModal from '@/hooks/modal/useLogoutModal';
 import { useGetMyProfile } from '@/hooks/profile/queries/useGetMyProfile';
 import useAuth from '@/store/useAuth';
 import theme from '@/theme';
@@ -11,15 +13,16 @@ const MyPageInfo = () => {
   const { isLogin } = useAuth();
   const { data, isPending, isError } = useGetMyProfile();
   const myProfile = data?.data?.data?.user;
-
-  const handleGoMate = () => {
-    navigate('/mate/management');
-  };
+  const logoutModal = useLogoutModal();
 
   return (
     <S.ProfileContainer>
+      <LogoutModal />
       {isLogin ? (
-        <S.ProfilePicture src={myProfile?.profileImage} alt="아" />
+        <S.ProfilePicture
+          src={myProfile?.profileImage}
+          alt={myProfile?.nickname}
+        />
       ) : (
         <S.ProfilePicture src={myPageImg?.ProfilePicture} />
       )}
@@ -27,16 +30,29 @@ const MyPageInfo = () => {
         {isLogin ? (
           <>
             <S.NickNameTypeContainer>
-              <h3> {myProfile?.nickname}</h3>
+              <h3>{myProfile?.nickname}</h3>
             </S.NickNameTypeContainer>
             <p>{myProfile?.email}</p>
             <p style={{ color: `${theme.COLOR.MAIN.BLACK}` }}>
               {myProfile?.introduction}
             </p>
-            <S.Mate onClick={handleGoMate}>
-              팔로워
-              <S.NumberOfPeople>{myProfile?.followers}</S.NumberOfPeople> 팔로잉
-              <S.NumberOfPeople>{myProfile?.followings}</S.NumberOfPeople>
+            <S.Mate onClick={() => navigate('/mate/management')}>
+              <S.FollowContainer>
+                <S.NumberOfPeople>
+                  <p>팔로워</p>
+                  {myProfile?.followers}
+                </S.NumberOfPeople>
+                <S.NumberOfPeople>
+                  <p>팔로잉</p> {myProfile?.followings}
+                </S.NumberOfPeople>
+                <S.Button
+                  onClick={e => {
+                    e.stopPropagation();
+                    logoutModal.onOpen();
+                  }}>
+                  로그아웃
+                </S.Button>
+              </S.FollowContainer>
             </S.Mate>
           </>
         ) : (
