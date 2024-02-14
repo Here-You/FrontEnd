bb960aca67d5903f4f7a491358619d1838c7b0a6
김용민 <dydals3440@gmail.com>
2024-02-15 04:08:04 +0900

    feat: Persit Zustand Login 상태 관리

--- src/store/useAuth.js
+++ src/store/useAuth.js
@@ -1,19 +1,32 @@
 import { create } from 'zustand';
 
-const useAuth = create(set => ({
-  isLogin: false,
-  login: token => {
-    localStorage.setItem('x-access-token', token);
-    set({ isLogin: true });
-  },
-  logout: () => {
-    localStorage.removeItem('x-access-token');
-    set({ isLogin: false });
-  },
-  revoke: () => {
-    localStorage.removeItem('x-access-token');
-    set({ isLogin: false });
-  },
-}));
+import { createJSONStorage, persist } from 'zustand/middleware';
+
+const JSONStorage = createJSONStorage(localStorage);
+
+const useAuth = create(
+  persist(
+    'authState',
+    set => ({
+      isLogin: false,
+      login: token => {
+        set({ isLogin: true });
+        localStorage.setItem('x-access-token', token);
+      },
+      logout: () => {
+        set({ isLogin: false });
+        localStorage.removeItem('x-access-token');
+      },
+      revoke: () => {
+        set({ isLogin: false });
+        localStorage.removeItem('x-access-token');
+      },
+    }),
+    {
+      name: 'authPersist',
+      getStorage: () => JSONStorage,
+    },
+  ),
+);
 
 export default useAuth;
