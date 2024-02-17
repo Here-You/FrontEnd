4e11d5bb30cdbb548d52c398fe2705dff8945e10
chaKK <cdin23@naver.com>
2024-02-18 02:25:40 +0900

    Feat: 메이트 탭 로그인 유무 확인

--- src/components/mate/Search.jsx
+++ src/components/mate/Search.jsx
@@ -1,12 +1,19 @@
 import * as S from './Search.style';
+import useAuth from '@/store/useAuth';
 
 const Search = ({ setSearchTerm }) => {
+  const { isLogin } = useAuth();
   return (
     <S.SearchContainer>
       <S.InputContainer>
         <S.InputText
-          placeholder="닉네임으로 메이트를 추가해보세요."
+          placeholder={
+            isLogin
+              ? '닉네임으로 메이트를 추가해보세요.'
+              : '로그인 후 메이트를 검색해보세요!'
+          }
           onChange={e => setSearchTerm(e.target.value)}
+          disabled={!isLogin}
         />
       </S.InputContainer>
     </S.SearchContainer>
