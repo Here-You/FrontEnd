ec994f0b0b2623fd69147377c9608a59451f4790
김용민 <dydals3440@gmail.com>
2024-02-18 01:02:43 +0900

    feat: 공개범위 설정 제거

--- src/components/mypage/SubCateGoryPage.jsx
+++ src/components/mypage/SubCateGoryPage.jsx
@@ -1,32 +1,17 @@
-import LogoutModal from '../modal/mypage/LogoutModal';
-import PublicScopeModal from '../modal/mypage/PublicScopeModal';
 import * as S from './SubCateGoryPage.style';
-import useLogoutModal from '@/hooks/modal/useLogoutModal';
-import usePublicScopeModal from '@/hooks/modal/usePublicScopeModal';
-import useAuth from '@/store/useAuth';
 
 const SubCateGoryPage = ({ header, listName }) => {
-  const publicScopeModal = usePublicScopeModal();
-
   return (
     <S.CategoryContainer>
       <h2>{header}</h2>
       {listName?.map(list => (
-        <S.Subcategory
-          key={list.id}
-          onClick={() => {
-            if (list.modal === true && list.id === 3) {
-              return publicScopeModal.onOpen();
-            }
-          }}>
+        <S.Subcategory key={list.id}>
           <S.LinkTo to={list.link}>
             <S.CategoryImg src={list.img} alt={list.title} />
             <p>{list.title}</p>
           </S.LinkTo>
         </S.Subcategory>
       ))}
-      <LogoutModal />
-      <PublicScopeModal />
     </S.CategoryContainer>
   );
 };
