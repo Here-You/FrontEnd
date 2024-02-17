d23f5b0cac57e1e63492fd83a54c647501c27107
cowboysj <su10jin11@khu.ac.kr>
2024-02-17 19:56:49 +0900

    Fix : 노션페이지 연결

--- src/constants/subcategory.js
+++ src/constants/subcategory.js
@@ -36,21 +36,21 @@ const SUPPORT = [
     id: 0,
     img: myPageImg.Question,
     title: '자주 묻는 질문',
-    link: 'https://www.notion.so/sally626/2364649c142d4ed6a65f8b8cc301a9a6?pvs=4',
+    link: 'https://sally626.notion.site/2364649c142d4ed6a65f8b8cc301a9a6',
     modal: false,
   },
   {
     id: 1,
     img: myPageImg.Bug,
     title: '버그 및 오류 제보',
-    link: 'https://www.notion.so/sally626/c86010e2a9f448eea449f246115ee8a7?pvs=4',
+    link: 'https://sally626.notion.site/c86010e2a9f448eea449f246115ee8a7',
     modal: false,
   },
   {
     id: 2,
     img: myPageImg.Chat,
     title: '의견 보내기',
-    link: 'https://www.notion.so/sally626/49fc0dfff6904eb9817d320eb7e4769d?pvs=4',
+    link: 'https://sally626.notion.site/49fc0dfff6904eb9817d320eb7e4769d',
     modal: false,
   },
 ];
@@ -60,7 +60,7 @@ const ETC = [
     id: 0,
     img: myPageImg.Sertificate,
     title: '개발자 소개',
-    link: 'https://www.notion.so/sally626/f566c3caed5747bb9a3fcfdc16b0857c?pvs=4',
+    link: 'https://sally626.notion.site/f566c3caed5747bb9a3fcfdc16b0857c',
     modal: false,
   },
   // {
