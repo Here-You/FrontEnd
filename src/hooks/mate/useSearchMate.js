2894add18b12e62786d4e70ab2b387ec4d2587bc
김용민 <dydals3440@gmail.com>
2024-02-18 00:37:23 +0900

    style: console.log 제거

---
+++ src/hooks/mate/useSearchMate.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+import { getSearchMate } from '@/apis/request/mate';
+
+export const useSearchMate = (searchTerm, cursor, take) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await getSearchMate(searchTerm, cursor, take);
+        const data = res.data.data.mates;
+        setData(data);
+      } catch (e) {
+        setError(e.message || '에러가 발생했습니다.');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [searchTerm, cursor, take]);
+
+  return { data, loading, error };
+};
---
+++ src/hooks/signature/useDeleteMySignature.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+import { deleteMySignature } from '@/apis/request/signature';
+
+// 시그니처 삭제하기
+export const useDeleteMySignature = signatureId => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await deleteMySignature(signatureId);
+        const data = res.data.data;
+        setData(data);
+      } catch (e) {
+        setError(e.message || '에러가 발생했습니다.');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { data, loading, error };
+};
