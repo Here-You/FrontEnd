c2cc194c258f364685c8a4857c4c3d7d3c652db9
chaKK <cdin23@naver.com>
2024-02-18 02:06:01 +0900

    Style: 시그니처 로그인 확인, 홈 화면 프로필 배너 스켈레톤 UI 제거

--- src/components/main/LoginBanner.jsx
+++ src/components/main/LoginBanner.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import * as S from './LoginBanner.style';
-import LoginSkeleton from './skeleton/LoginSkeleton';
 import profileImg from '/images/main/profileImg.svg';
 import rightIcon from '/images/main/right.svg';
 import { useGetMyProfile } from '@/hooks/profile/queries/useGetMyProfile';
@@ -20,34 +19,30 @@ export default function LoginBanner({ isLogin }) {
 
   return (
     <>
-      {pendingState ? (
-        <LoginSkeleton />
-      ) : (
-        <S.Wrapper>
-          <S.Container>
-            {isLogin ? (
-              <>
-                <S.Img src={myProfile?.profileImage} />
-                <S.TextContainer>
-                  <S.Text1>{myProfile?.nickname}님 환영합니다🪐</S.Text1>
-                  <S.ProfileContainer onClick={() => navigate('/mypage')}>
-                    <S.Text2>내 프로필 가기</S.Text2>
-                    <img src={rightIcon} />
-                  </S.ProfileContainer>
-                </S.TextContainer>
-              </>
-            ) : (
-              <>
-                <S.Img src={profileImg} />
-                <S.TextWrapper onClick={() => navigate('/login')}>
-                  <S.Text3>3초만에 로그인하기 </S.Text3>
-                  <img width="24px" src={rightIcon} />
-                </S.TextWrapper>
-              </>
-            )}
-          </S.Container>
-        </S.Wrapper>
-      )}
+      <S.Wrapper>
+        <S.Container>
+          {isLogin ? (
+            <>
+              <S.Img src={myProfile?.profileImage} />
+              <S.TextContainer>
+                <S.Text1>{myProfile?.nickname}님 환영합니다🪐</S.Text1>
+                <S.ProfileContainer onClick={() => navigate('/mypage')}>
+                  <S.Text2>내 프로필 가기</S.Text2>
+                  <img src={rightIcon} />
+                </S.ProfileContainer>
+              </S.TextContainer>
+            </>
+          ) : (
+            <>
+              <S.Img src={profileImg} />
+              <S.TextWrapper onClick={() => navigate('/login')}>
+                <S.Text3>3초만에 로그인하기 </S.Text3>
+                <img width="24px" src={rightIcon} />
+              </S.TextWrapper>
+            </>
+          )}
+        </S.Container>
+      </S.Wrapper>
     </>
   );
 }
