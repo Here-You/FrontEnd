764c71e541fa3da74a80b92b3c5624d77e65979e
chaKK <cdin23@naver.com>
2024-02-18 03:12:20 +0900

    Feat: 마이페이지 유저 정보 로그인 유무 확인

--- src/components/mypage/Category.jsx
+++ src/components/mypage/Category.jsx
@@ -1,10 +1,14 @@
 import SubCateGoryPage from './SubCateGoryPage';
 import { ETC, SUPPORT, USER_SETTING } from '@/constants/subcategory';
+import useAuth from '@/store/useAuth';
 
 const Category = () => {
+  const { isLogin } = useAuth();
   return (
     <>
-      <SubCateGoryPage listName={USER_SETTING} header={'유저 설정'} />
+      {isLogin && (
+        <SubCateGoryPage listName={USER_SETTING} header={'유저 설정'} />
+      )}
       <SubCateGoryPage listName={SUPPORT} header={'지원'} />
       <SubCateGoryPage listName={ETC} header={'기타'} />
     </>
