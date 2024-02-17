f394b5c9a79ecdd8a56894ed9afea7957e736cda
chaKK <cdin23@naver.com>
2024-02-17 18:59:04 +0900

    Style: 하루일지 상세 페이지 스켈레톤 UI 적용

--- src/pages/dailyRecord/DailyRecordPage.jsx
+++ src/pages/dailyRecord/DailyRecordPage.jsx
@@ -3,6 +3,7 @@ import { useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import * as S from './DailyRecordPage.style';
+import DailyRecordEditSkeleton from './edit/skeleton/DailyRecordEditSkeleton';
 import PenGreen from '/icons/PenGreen.svg';
 import { MOOD_ICON_LIST, WEATHER_ICON_LIST } from '@/constants/dailyRecord';
 import { useGetDiary } from '@/hooks/home/useGetDiary';
@@ -18,49 +19,51 @@ const DailyRecordPage = () => {
     return { date };
   }, [diaryData]);
 
-  if (loading) {
-    return <div>로딩 중입니다..</div>;
-  }
-
   if (error) {
     return <ErrorPage />;
   }
 
   return (
-    <S.Container>
-      <S.DateContainer>
-        <S.YearText>{Date.date && Date?.date[0]}</S.YearText>
-        <S.DateText>
-          {Date.date && Date?.date[1]}, {Date.date && Date?.date[2]}
-        </S.DateText>
-      </S.DateContainer>
-      <S.RecordContainer>
-        <S.RecordImageContainer>
-          <S.PreviewImage src={diaryData?.imageUrl} />
-        </S.RecordImageContainer>
-        <S.LocationText>{diaryData?.place}</S.LocationText>
-        <S.TitleText>{diaryData?.title}</S.TitleText>
+    <>
+      {loading ? (
+        <DailyRecordEditSkeleton />
+      ) : (
+        <S.Container>
+          <S.DateContainer>
+            <S.YearText>{Date.date && Date?.date[0]}</S.YearText>
+            <S.DateText>
+              {Date.date && Date?.date[1]}, {Date.date && Date?.date[2]}
+            </S.DateText>
+          </S.DateContainer>
+          <S.RecordContainer>
+            <S.RecordImageContainer>
+              <S.PreviewImage src={diaryData?.imageUrl} />
+            </S.RecordImageContainer>
+            <S.LocationText>{diaryData?.place}</S.LocationText>
+            <S.TitleText>{diaryData?.title}</S.TitleText>
 
-        <S.WeatherContainer>
-          {WEATHER_ICON_LIST.map(item => {
-            if (item.iconName === diaryData?.weather) {
-              return <S.Icon key={item.id} src={item.iconUrl} />;
-            }
-          })}
-          {MOOD_ICON_LIST.map(item => {
-            if (item.iconName === diaryData?.mood) {
-              return <S.Icon key={item.id} src={item.iconUrl} />;
-            }
-          })}
-        </S.WeatherContainer>
-        <S.ContentText>{diaryData?.content}</S.ContentText>
-      </S.RecordContainer>
+            <S.WeatherContainer>
+              {WEATHER_ICON_LIST.map(item => {
+                if (item.iconName === diaryData?.weather) {
+                  return <S.Icon key={item.id} src={item.iconUrl} />;
+                }
+              })}
+              {MOOD_ICON_LIST.map(item => {
+                if (item.iconName === diaryData?.mood) {
+                  return <S.Icon key={item.id} src={item.iconUrl} />;
+                }
+              })}
+            </S.WeatherContainer>
+            <S.ContentText>{diaryData?.content}</S.ContentText>
+          </S.RecordContainer>
 
-      <S.AddButton
-        to={`/dailyrecord/${diaryData?.id}/edit?scheduleId=${scheduleId}`}>
-        <S.PenIcon src={PenGreen} />
-      </S.AddButton>
-    </S.Container>
+          <S.AddButton
+            to={`/dailyrecord/${diaryData?.id}/edit?scheduleId=${scheduleId}`}>
+            <S.PenIcon src={PenGreen} />
+          </S.AddButton>
+        </S.Container>
+      )}
+    </>
   );
 };
 
