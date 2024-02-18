2dc37ab14d5a4c74f0daf5e1fcca9120a83ab2c8
김용민 <dydals3440@gmail.com>
2024-02-19 00:21:13 +0900

    Feat: FollowRedirectPage 제작

--- src/pages/explore/followRedirect/FollowRedirectPage.jsx
+++ src/pages/explore/followRedirect/FollowRedirectPage.jsx
@@ -1,17 +1,13 @@
-import { useNavigate } from 'react-router-dom';
-
 import * as S from './FollowRedirectPage.style';
 import AirplaneImg from '/images/onboarding1.svg';
 
 const FollowRedirectPage = () => {
-  const navigate = useNavigate();
   return (
     <S.PageContainer>
       <S.ContentContainer>
         <img src={AirplaneImg} />
         <S.Text>로그인하고 내 메이트와</S.Text>
         <S.Text>새로운 여행 기록을 확인해보세요!</S.Text>
-        <S.Button onClick={() => navigate('/login')}>로그인 하기</S.Button>
       </S.ContentContainer>
     </S.PageContainer>
   );
