456bc8f88f331a970dd9098346d726989a1acb36
kimjunha <tto9011@naver.com>
2024-02-18 16:14:05 +0900

    [Feat]:탐색 페이지도 msw연결

--- src/mocks/handlers/explore.js
+++ src/mocks/handlers/explore.js
@@ -6,138 +6,72 @@ import { baseURL } from '@/apis/api';
 import { API_URL } from '@/constants/path';
 
 export const ExploreHandlers = [
-  // 탐색탭_메인 화면
-  http.get(`${baseURL}${API_URL.SEARCH_SIGNATURE}`, ({ request, params }) => {
-    const url = new URL(request.url);
-    const search = url.searchParams.get('search');
-    if (search) {
+  // 탐색탭_핫 시그니처
+  http.get(
+    `${baseURL}${API_URL.SEARCH_SIGNATURE}/hot`,
+    ({ request, params }) => {
       return HttpResponse.json({
-        status: 200,
+        timestamp: '2024-02-05T20:12:21.562Z',
+        code: 'OK',
         success: true,
-        message: `탑색탭 메인 ${search} 불러오기 성공`,
+        message: '탐색탭 메인 화면 가져오기 성공',
         data: {
-          hot: [
+          covers: [
             {
-              _id: 3,
-              title: `${search}`,
-              date: '24/01/19',
-              image:
-                'https://images.unsplash.com/photo-1682686581498-5e85c7228119?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              like: 5,
-              user: {
-                name: `${search}`,
-                image:
-                  'https://plus.unsplash.com/premium_photo-1705352059948-e5512efca860?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              },
+              _id: 5,
+              title: '강남역 근처 카페 모음',
+              liked: 0,
+              userName: '써니',
+              date: '2024.02.06',
+              image: null,
+              userImage: 'imagekey1',
             },
             {
-              _id: 4,
-              title: `${search} 시그니처 제목2`,
-              date: '24/01/19',
-              image:
-                'https://images.unsplash.com/photo-1682686581498-5e85c7228119?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              like: 5,
-              user: {
-                name: `${search}`,
-                image:
-                  'https://plus.unsplash.com/premium_photo-1705352059948-e5512efca860?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              },
-            },
-          ],
-          new: [
-            {
-              _id: 1,
-              title: `${search} 시그니처 제목3`,
-              date: '24/01/19',
-              image:
-                'https://images.unsplash.com/photo-1682686581498-5e85c7228119?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              like: 5,
-              user: {
-                name: `${search}`,
-                image:
-                  'https://plus.unsplash.com/premium_photo-1705352059948-e5512efca860?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              },
-            },
-            {
-              _id: 2,
-              title: `${search} 시그니처 제목4`,
-              date: '24/01/19',
-              image:
-                'https://images.unsplash.com/photo-1682686581498-5e85c7228119?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              like: 5,
-              user: {
-                name: `${search}`,
-                image:
-                  'https://plus.unsplash.com/premium_photo-1705352059948-e5512efca860?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              },
+              _id: 5,
+              title: '강남역 근처 카페 모음',
+              liked: 0,
+              userName: '써니',
+              date: '2024.02.06',
+              image: null,
+              userImage: 'imagekey1',
             },
           ],
         },
       });
-    } else {
+    },
+  ),
+  // 탐색탭_최신 시그니처
+  http.get(
+    `${baseURL}${API_URL.SEARCH_SIGNATURE}/new`,
+    ({ request, params }) => {
       return HttpResponse.json({
-        status: 200,
+        timestamp: '2024-02-05T20:12:21.562Z',
+        code: 'OK',
         success: true,
-        message: `탑색탭 메인 전체 불러오기 성공`,
+        message: '탐색탭 메인 화면 가져오기 성공',
         data: {
-          hot: [
+          covers: [
             {
-              _id: 3,
-              title: `전체 시그니처 제목1`,
-              date: '24/01/19',
-              image:
-                'https://images.unsplash.com/photo-1682686581498-5e85c7228119?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              like: 5,
-              user: {
-                name: '닉네임',
-                image:
-                  'https://plus.unsplash.com/premium_photo-1705352059948-e5512efca860?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              },
+              _id: 5,
+              title: '강남역 근처 카페 모음',
+              liked: 0,
+              userName: '써니',
+              date: '2024.02.06',
+              image: null,
+              userImage: 'imagekey1',
             },
             {
-              _id: 4,
-              title: `전체 시그니처 제목2`,
-              date: '24/01/19',
-              image:
-                'https://images.unsplash.com/photo-1682686581498-5e85c7228119?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              like: 5,
-              user: {
-                name: '닉네임',
-                image:
-                  'https://plus.unsplash.com/premium_photo-1705352059948-e5512efca860?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              },
-            },
-          ],
-          new: [
-            {
-              _id: 1,
-              title: `전체 시그니처 제목3`,
-              date: '24/01/19',
-              image:
-                'https://images.unsplash.com/photo-1682686581498-5e85c7228119?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              like: 5,
-              user: {
-                name: '닉네임',
-                image:
-                  'https://plus.unsplash.com/premium_photo-1705352059948-e5512efca860?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              },
-            },
-            {
-              _id: 2,
-              title: `전체 시그니처 제목4`,
-              date: '24/01/19',
-              image:
-                'https://images.unsplash.com/photo-1682686581498-5e85c7228119?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              like: 5,
-              user: {
-                name: '닉네임',
-                image:
-                  'https://plus.unsplash.com/premium_photo-1705352059948-e5512efca860?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              },
+              _id: 5,
+              title: '강남역 근처 카페 모음',
+              liked: 0,
+              userName: '써니',
+              date: '2024.02.06',
+              image: null,
+              userImage: 'imagekey1',
             },
           ],
         },
       });
-    }
-  }),
+    },
+  ),
 ];
